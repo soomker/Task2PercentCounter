b806ec5f5ddf0fccdc0f3ac63328c5e290ad778f
soomker <soomker@gmail.com>
2016-07-09 23:58:51 +0300

    Fix1

---
+++ PercentCounter/PercentCounter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercentCounter
+{
+    static class Counter
+    {
+       static Dictionary<string, int> percents = new Dictionary<string, int> {{"USA",10}, {"Ukraine",15}, {"Norway",30}};
+        string country = String.Empty;
+        
+       public static int GetPercent(string country)
+        {
+            try
+            {
+              return percents[country];
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
